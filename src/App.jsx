da4e46fa388549e8f95e9282a642ca1e480ba2e4
mrsherpacode <sherpacoding@gmail.com>
2025-09-20 00:27:35 -0500

    created global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
 // Here, i'm using styled components  css for styling
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 // name starts with  capital letter cuz its a component
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: orange;
 `;
-// reusable  button
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 5px;
-  background-color: red;
-  color: white;
-  margin-right: 20px;
-`;
-// input elements
-const Input = styled.input`
-  border: 1px solid #333;
-  border-radius: 5px;
-  padding: 1rem 2rem;
-`;
+
 // styling  App component
 const StyledApp = styled.div`
   background-color: gray;
@@ -30,12 +17,16 @@ const StyledApp = styled.div`
 `;
 function App() {
   return (
-    <StyledApp>
-      <H1>Hello</H1>
-      <Button onClick={() => alert("check in")}>check in</Button>
-      <Button onClick={() => alert("check out")}>check out</Button>
-      <Input type="number" placeholder="guest number" />
-    </StyledApp>
+    <>
+      {/*This  GlobalStyles component does not accept any children and should be a sibling to the styled app component.  */}
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Hello</H1>
+        <Button onClick={() => alert("check in")}>check in</Button>
+        <Button onClick={() => alert("check out")}>check out</Button>
+        <Input type="number" placeholder="guest number" />
+      </StyledApp>
+    </>
   );
 }
 
---
+++ src/ui/Input.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+//Reusable input element component
+const Input = styled.input`
+  border: 1px solid va(--color-gray-300);
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-sm);
+  padding: 1rem 2rem;
+`;
+
+export default Input;
