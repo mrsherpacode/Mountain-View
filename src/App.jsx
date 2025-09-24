5b5f334cff9d3fc7896a7e4ab5f47762368e6bcb
mrsherpacode <sherpacoding@gmail.com>
2025-09-24 14:58:04 -0500

    installed react query and used in the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Dashboard from "../src/pages/Dashboard";
 import GlobalStyles from "../src/styles/GlobalStyles";
 import Cabins from "../src/pages/Cabins";
@@ -9,11 +11,21 @@ import Users from "../src/pages/Users";
 import Settings from "../src/pages/Settings";
 import PageNotFound from "../src/pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+
+// Setting Up React Query
+const queryClient = new QueryClient({
+  defaultOptions: {
+    staleTime: 60 * 1000, // 1 minutes in milliseconds
+  },
+});
 // Declaritively Browsing route
 function App() {
   return (
     <BrowserRouter>
-      <>
+      {/*Providing the QueryClient(reactQuery) to the entire Application*/}
+      <QueryClientProvider client={queryClient}>
+        {/* Integrating React Query Dev Tools */}
+        <ReactQueryDevtools />
         {/* Global css style */}
         <GlobalStyles />
         <Routes>
@@ -31,7 +43,7 @@ function App() {
           <Route path="login" element={<Login />} />
           <Route path="pageNotFound" element={<PageNotFound />} />
         </Routes>
-      </>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
