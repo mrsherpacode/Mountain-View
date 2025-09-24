f226618c6d5759ec0598b67e490a854038f60a7f
mrsherpacode <sherpacoding@gmail.com>
2025-09-24 00:26:13 -0500

    created back-end database with supabase and connected it with my project

---
+++ src/services/apiCabins.js
@@ -0,0 +1,12 @@
+//Creating a Service to Query Cabins
+
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins can't be loaded");
+  }
+  return data;
+}
