a6fb80a86a0652abf47a30aa426a4599867fff5c
mrsherpacode <sherpacoding@gmail.com>
2025-11-27 14:48:43 -0600

    signup form is created and validated

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,14 +1,17 @@
 // This is a custom React hook that wraps the login functionality using React Query's useMutation.
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: () => {
+    onSuccess: (data) => {
+      // This saves user data to cache, then all components get it instantly from there instead of making new API calls!
+      queryClient.setQueryData(["user"], data.user);
       navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
