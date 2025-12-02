a9ff81bfca44ec0326f6f5cb7c8c87f98aef3485
mrsherpacode <sherpacoding@gmail.com>
2025-12-02 14:39:01 -0600

    Built the application header containing username, avatar, and a small menu.

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,15 +1,21 @@
 import styled from "styled-components";
-import Logout from "../features/authentication/Logout";
+import HeaderMenu from "./HeaderMenu";
+import UserAvatar from "../features/authentication/UserAvatar";
 
 const StyledHeader = styled.header`
   background-color: var(--color-gray-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-gray-100);
+  display: flex;
+  align-items: center;
+  gap: 2.5rem;
+  justify-content: flex-end;
 `;
 function Header() {
   return (
     <StyledHeader>
-      <Logout />
+      <UserAvatar />
+      <HeaderMenu />
     </StyledHeader>
   );
 }
